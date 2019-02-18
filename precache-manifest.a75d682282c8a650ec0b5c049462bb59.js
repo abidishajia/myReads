a603ca025da88af59ceec1848bf372b77c847dd8
abidishajia <abidishajia@gmail.com>
2019-02-17 23:03:23 -0800

    Updates

---
+++ precache-manifest.a75d682282c8a650ec0b5c049462bb59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "27d1eb6964042f3c50bc",
+    "url": "/myReads/static/js/runtime~main.27d1eb69.js"
+  },
+  {
+    "revision": "12f4b84b7abbb7e21f41",
+    "url": "/myReads/static/js/main.12f4b84b.chunk.js"
+  },
+  {
+    "revision": "c5dd125d84915995889e",
+    "url": "/myReads/static/js/1.c5dd125d.chunk.js"
+  },
+  {
+    "revision": "12f4b84b7abbb7e21f41",
+    "url": "/myReads/static/css/main.5931460f.chunk.css"
+  },
+  {
+    "revision": "1c5464570c77f784919ce06060878232",
+    "url": "/myReads/index.html"
+  }
+];
